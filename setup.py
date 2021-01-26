02fb0dcca8e9b908a31243c5066ba5c526075506
JezBurch1984 <78015294+JezBurch1984@users.noreply.github.com>
2021-01-26 00:32:58 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,14 @@
 from distutils.core import setup
 setup(
-  name = 'xramdp,',         # How you named your package folder (MyLib)
+  name = 'xrandom,',         # How you named your package folder (MyLib)
   packages = ['xrandom'],   # Chose the same as "name"
-  version = '0.3',      # Start with a small number and increase it with every change you make
+  version = '0.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'legacy random',   # Give a short description about your library
   author = 'Jez Burch',                   # Type in your name
   author_email = 'throwawayemail49572@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/JezBurch1984/xrandom/',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/JezBurch1984/xrandom/archive/0.3.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/JezBurch1984/xrandom/archive/0.4.tar.gz',    # I explain this later on
   keywords = ['legacy', 'random'],   # Keywords that define your package best
   install_requires=[]
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.8',
-  ],
 )
