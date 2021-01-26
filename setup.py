62abc2171fe86cf50974e5960645f7184a8985a9
JezBurch1984 <78015294+JezBurch1984@users.noreply.github.com>
2021-01-26 00:22:35 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'xramdp,',         # How you named your package folder (MyLib)
   packages = ['xrandom'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'legacy random',   # Give a short description about your library
   author = 'Jez Burch',                   # Type in your name
   author_email = 'throwawayemail49572@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/JezBurch1984/xrandom/',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/JezBurch1984/xrandom/archive/0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/JezBurch1984/xrandom/archive/0.3.tar.gz',    # I explain this later on
   keywords = ['legacy', 'random'],   # Keywords that define your package best
   install_requires=[]
   classifiers=[
